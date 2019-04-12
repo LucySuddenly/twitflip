b715bb559df98deae3d698842160a018040cb898
halented <hallie.dunn1@gmail.com>
2019-04-12 08:57:54 -0700

    duh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CollectionContainer from './components/CollectionContainer'
 import SearchContainer from './components/SearchContainer'
