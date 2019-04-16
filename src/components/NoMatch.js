70c7da685f1e0b31b522d2ebc7bbbe68e6a479e6
halented <hallie.dunn1@gmail.com>
2019-04-16 15:59:47 -0700

    actual 404 message

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -4,7 +4,7 @@ class NoMatch extends Component {
     render() {
         return (
             <div>
-                No such page exists, so here's this one
+                <h1>404</h1>
                 <img src="https://i.ytimg.com/vi/UM-dvlLONh4/maxresdefault.jpg" alt="pull the trigger, piglet"></img>
             </div>
         );
